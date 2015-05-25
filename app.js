c4aef37fc9f38e435b14ba7965b275df4036143a
Steve007 <stevenag006@hotmail.com>
2015-05-25 01:25:10 -0400

    config file change

--- app.js
+++ app.js
@@ -22,65 +22,65 @@ var MCKEY = process.env.MC_KEY || '8b8bff773968e11ac3881a74bb8bef66-us10'
 
 
 
-// var Client = require('pg-native');
-// var conString = process.env.DATABASE_URL;
+var Client = require('pg-native');
+var conString = process.env.DATABASE_URL;
 
 
-// var client = new Client();
-// var queryString = "CREATE TABLE IF NOT EXISTS users \
-//   ( \
-//     ID             SERIAL PRIMARY KEY     NOT NULL, \
-//     NAME           TEXT,    \
-//     EMAIL          TEXT    NOT NULL, \
-//     PHONE          TEXT,    \
-//     IP             TEXT,    \
-//     CREATE_DATE    TIMESTAMP without time zone default (now() at time zone 'utc'), \
-//     ADDRESS        CHAR(50), \
-//     POSTAL         CHAR(50), \
-//     CITY           CHAR(50) \
-//   ); \
-//   CREATE TABLE IF NOT EXISTS orders \
-//   ( \
-//     ID             SERIAL PRIMARY KEY     NOT NULL, \
-//     USER_ID        INT,     \
-//     STRIPE_CID     TEXT,     \
-//     TOKEN          TEXT, \
-//     PRODUCT_NAME   TEXT, \
-//     MAIL           BOOL, \
-//     SUBTOTAL       INT, \
-//     TOTAL          INT, \
-//     STATUS         TEXT, \
-//     CREATE_DATE    TIMESTAMP without time zone default (now() at time zone 'utc'), \
-//     APPROVE_DATE   TIMESTAMP without time zone default (now() at time zone 'utc') \
-//   ); \
-//   CREATE TABLE IF NOT EXISTS order_details \
-//   ( \
-//     ID             SERIAL PRIMARY KEY    NOT NULL, \
-//     ORDER_ID       INT,     \
-//     COMPANY_NAME   TEXT,     \
-//     POSITION       TEXT, \
-//     INDUSTRY       TEXT, \
-//     YEARS          TEXT, \
-//     WEBSITE        TEXT, \
-//     IMPORTANT      TEXT, \
-//     PURPOSE        TEXT, \
-//     CUSTOMER       TEXT \
-//   ); \
-// "
+var client = new Client();
+var queryString = "CREATE TABLE IF NOT EXISTS users \
+  ( \
+    ID             SERIAL PRIMARY KEY     NOT NULL, \
+    NAME           TEXT,    \
+    EMAIL          TEXT    NOT NULL, \
+    PHONE          TEXT,    \
+    IP             TEXT,    \
+    CREATE_DATE    TIMESTAMP without time zone default (now() at time zone 'utc'), \
+    ADDRESS        CHAR(50), \
+    POSTAL         CHAR(50), \
+    CITY           CHAR(50) \
+  ); \
+  CREATE TABLE IF NOT EXISTS orders \
+  ( \
+    ID             SERIAL PRIMARY KEY     NOT NULL, \
+    USER_ID        INT,     \
+    STRIPE_CID     TEXT,     \
+    TOKEN          TEXT, \
+    PRODUCT_NAME   TEXT, \
+    MAIL           BOOL, \
+    SUBTOTAL       INT, \
+    TOTAL          INT, \
+    STATUS         TEXT, \
+    CREATE_DATE    TIMESTAMP without time zone default (now() at time zone 'utc'), \
+    APPROVE_DATE   TIMESTAMP without time zone default (now() at time zone 'utc') \
+  ); \
+  CREATE TABLE IF NOT EXISTS order_details \
+  ( \
+    ID             SERIAL PRIMARY KEY    NOT NULL, \
+    ORDER_ID       INT,     \
+    COMPANY_NAME   TEXT,     \
+    POSITION       TEXT, \
+    INDUSTRY       TEXT, \
+    YEARS          TEXT, \
+    WEBSITE        TEXT, \
+    IMPORTANT      TEXT, \
+    PURPOSE        TEXT, \
+    CUSTOMER       TEXT \
+  ); \
+"
 
 
-// client.connect(conString, function(err) {
-//   if(err) {
-//     return console.error('could not connect to postgres', err);
-//   }
-//   client.query(queryString, function(err, result) {
-//     if(err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(result);
-//     client.end();
-//   });
-// });
+client.connect(conString, function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query(queryString, function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result);
+    client.end();
+  });
+});
 
 
 mc = new mcapi.Mailchimp(MCKEY);
