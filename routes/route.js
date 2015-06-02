cfa74e66d2441b8f184506e611229dc04debb94a
Steve007 <stevenag006@hotmail.com>
2015-06-01 22:35:50 -0400

    https

--- routes/route.js
+++ routes/route.js
@@ -131,9 +131,6 @@ function createOrderDetail(req, id) {
 
 }
 //Routes
-router.get('*',function(req,res){  
-  res.redirect('https://designedforresult.com'+req.url)
-});
 
 router.post('/checkout', function (req, res, next) {
   console.log(req.body)
