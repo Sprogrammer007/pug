cb9c3dec6c8690d97625244b8edc3121f67d57b1
Steve007 <stevenag006@hotmail.com>
2015-08-04 12:55:18 -0400

    survey alpha

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ mailer.extend(app, {
 });
 
 // if (process.env.RUN_DB === 'true') {
-  db.init();
-  db.dbMigrate();
+db.init();
+// db.dbMigrate();
 // }
 
 app.locals.default_description = "We love beautiful designs too, but beauty alone doesn\'t always deliver result for your business. Designed for Result will help you get clear on the objecti";
